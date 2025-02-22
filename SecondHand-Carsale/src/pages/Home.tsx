dc08cecfe50f1a77fe097e628175fee58f7b156f
VIJAYANAARAAYANAN <vijaybalaram05@gmail.com>
2025-02-23 00:30:07 +0530

    NewComps

--- SecondHand-Carsale/src/pages/Home.tsx
+++ SecondHand-Carsale/src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import "../PageCss/Home.css";
 import backgroundImage from "../assets/CarHomebanner.svg";
 import React, { useState } from "react";
-import FeatureListing from "../components/FeatureListing.tsx"
+import FeatureListing from "../components/FeatureListing.tsx";
+import Explore from "../components/Explore.tsx";
+import GetInTouch from "../components/GetinTouch.tsx";
+import Roadmap from "../components/RoadMap.tsx";
+import Latest from "../components/Latest.tsx";
 const Home = () => {
   const [minValue, setMinValue] = useState(1000);
   const [maxValue, setMaxValue] = useState(5000);
@@ -18,100 +22,102 @@ const Home = () => {
   };
 
   const minPos = ((minValue - 1000) / (5000 - 1000)) * 100;
-  const maxPos = ((maxValue - 1000) / (5000 - 1000)) * 100 - 2 ;
+  const maxPos = ((maxValue - 1000) / (5000 - 1000)) * 100 - 2;
 
   return (
     <>
-    <div className="home-container">
-      <div className="background-overlay"></div>
-      <div className="content">
-        <div className="left-content">
-          <h1>
-            Find Quality-Assured Cars Tailored to Your Budget and Preferences
-          </h1>
-          <p>
-            Browse a Wide Range of Certified Used Cars from Trusted Dealers and
-            Private Sellers
-          </p>
-          <div className="button-rating">
-            <button className="book-btn">Book My Car</button>
-            <div className="rating">
-              ⭐⭐⭐⭐⭐ <span>Working with 50+ Happy members</span>
+      <div className="home-container">
+        <div className="background-overlay"></div>
+        <div className="content">
+          <div className="left-content">
+            <h1>
+              Find Quality-Assured Cars Tailored to Your Budget and Preferences
+            </h1>
+            <p>
+              Browse a Wide Range of Certified Used Cars from Trusted Dealers
+              and Private Sellers
+            </p>
+            <div className="button-rating">
+              <button className="book-btn">Book My Car</button>
+              <div className="rating">
+                ⭐⭐⭐⭐⭐ <span>Working with 50+ Happy members</span>
+              </div>
             </div>
           </div>
-
-        </div>
-        <div className="right-content">
-          <div className="car-filter">
-            <div className="tabs">
-              <button
-                className={activeTab === "New cars" ? "active" : ""}
-                onClick={() => setActiveTab("New cars")}
-              >
-                New cars
-              </button>
-              <button
-                className={activeTab === "Used cars" ? "active" : ""}
-                onClick={() => setActiveTab("Used cars")}
-              >
-                Used cars
-              </button>
-            </div>
-            <input type="text" placeholder="Make" className="input-field" />
-            <input type="text" placeholder="Models" className="input-field" />
-
-            <div className="dual-range-slider">
-              <div className="slider-track"></div>
-              <div
-                className="slider-range"
-                style={{
-                  left: `${minPos}%`,
-                  right: `${100 - maxPos}%`,
-                }}
-              ></div>
-              <div className="thumb-group">
-                <input
-                  type="range"
-                  min="1000"
-                  max="5000"
-                  value={minValue}
-                  onChange={handleMinChange}
-                  className="thumb thumb-left"
-                />
-                <span
-                  className="thumb-value"
-                  style={{ left: `calc(${minPos}% + 8px)` }}
+          <div className="right-content">
+            <div className="car-filter">
+              <div className="tabs">
+                <button
+                  className={activeTab === "New cars" ? "active" : ""}
+                  onClick={() => setActiveTab("New cars")}
                 >
-                  {minValue.toLocaleString()}
-                </span>
+                  New cars
+                </button>
+                <button
+                  className={activeTab === "Used cars" ? "active" : ""}
+                  onClick={() => setActiveTab("Used cars")}
+                >
+                  Used cars
+                </button>
               </div>
-              <div className="thumb-group">
-                <input
-                  type="range"
-                  min="1000"
-                  max="5000"
-                  value={maxValue}
-                  onChange={handleMaxChange}
-                  className="thumb thumb-right"
-                />
-                <span className="thumb-value" style={{ left: `${maxPos}%` }}>
-                  {maxValue.toLocaleString()}
-                </span>
-              </div>
-            </div>
+              <input type="text" placeholder="Make" className="input-field" />
+              <input type="text" placeholder="Models" className="input-field" />
 
-            <input type="text" placeholder="Body" className="input-field" />
-            <button className="search-btn">2334 Cars</button>
+              <div className="dual-range-slider">
+                <div className="slider-track"></div>
+                <div
+                  className="slider-range"
+                  style={{
+                    left: `${minPos}%`,
+                    right: `${100 - maxPos}%`,
+                  }}
+                ></div>
+                <div className="thumb-group">
+                  <input
+                    type="range"
+                    min="1000"
+                    max="5000"
+                    value={minValue}
+                    onChange={handleMinChange}
+                    className="thumb thumb-left"
+                  />
+                  <span
+                    className="thumb-value"
+                    style={{ left: `calc(${minPos}% + 8px)` }}
+                  >
+                    {minValue.toLocaleString()}
+                  </span>
+                </div>
+                <div className="thumb-group">
+                  <input
+                    type="range"
+                    min="1000"
+                    max="5000"
+                    value={maxValue}
+                    onChange={handleMaxChange}
+                    className="thumb thumb-right"
+                  />
+                  <span className="thumb-value" style={{ left: `${maxPos}%` }}>
+                    {maxValue.toLocaleString()}
+                  </span>
+                </div>
+              </div>
+
+              <input type="text" placeholder="Body" className="input-field" />
+              <button className="search-btn">2334 Cars</button>
+            </div>
           </div>
         </div>
+        <div className="Welcome-text">
+          <p>Welcome</p>
+        </div>
       </div>
-      <div className="Welcome-text">
-        <p>Welcome</p>
-      </div>
-     
-    </div>
-     <FeatureListing/>
-     </>
+      <Explore />
+      <FeatureListing />
+      <GetInTouch />
+      <Roadmap/>
+      <Latest/>
+    </>
   );
 };
 
